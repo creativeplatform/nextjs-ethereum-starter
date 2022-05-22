014b408ec72cf1195c0009fecc7fd07e7009f226
G2 <bracy.gawain2014@gmail.com>
2022-05-22 10:25:00 -0400

    Revert "g2 updates"

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -8,7 +8,6 @@ import YourContract from '../artifacts/contracts/YourContract.sol/YourContract.j
 import Layout from '../components/layout/Layout'
 import { YourContract as YourContractType } from '../types/typechain'
 import axios from 'axios';
-import { result } from 'lodash'
 
 /**
  * Constants & Helpers
@@ -22,7 +21,7 @@ const ROPSTEN_CONTRACT_ADDRESS = '0x6b61a52b1EA15f4b8dB186126e980208E1E18864'
 
 export interface Item {
   avatar: string;
-  value: string;
+  name: string;
 }
 /**
  * Prop Types
@@ -135,11 +134,11 @@ function HomeIndex(): JSX.Element {
   const [state, dispatch] = useReducer(reducer, initialState)
   const { account, chainId, library } = useEthers()
 
-  const [pickerItems, setPickerItems] = React.useState(artists.results);
+  const [pickerItems, setPickerItems] = React.useState(artists);
   const [selectedItems, setSelectedItems] = React.useState([]);
 
-  const handleCreateItem = (item: Item) => {
-    setPickerItems([]);
+  const handleCreateItem = (item) => {
+    setPickerItems ((curr) => [...curr, item]);
     setSelectedItems((curr) => [...curr, item]);
   };
 
