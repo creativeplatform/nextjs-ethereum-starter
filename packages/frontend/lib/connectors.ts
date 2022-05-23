2cf3c4bf592c02bced2648dd3ae4cb81c76030f8
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-05-22 22:18:33 -0400

    update connectors

--- packages/frontend/lib/connectors.ts
+++ packages/frontend/lib/connectors.ts
@@ -7,13 +7,13 @@ const RPC_URLS: { [chainId: number]: string } = {
   4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
 }
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
+  rpc: { 42: RPC_URLS[42], 80001: RPC_URLS[80001] },
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
 })
 
 export const walletlink = new WalletLinkConnector({
-  url: RPC_URLS[1],
+  url: RPC_URLS[42],
   appName: 'Coinbase',
-  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001],
+  supportedChainIds: [ 42, 80001],
 })
